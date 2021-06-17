68ef0c3cdaace0faedfde6281868147b35754f10
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-06-17 15:13:32 +0300

    Таймер останавливается при значении 00:00:00:00

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ class CountdownTimer {
   }
 
   getTimerComponents(time) {
+    const total = time;
     const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
     const hours = this.pad(
       Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
@@ -18,15 +19,19 @@ class CountdownTimer {
     const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
     const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
-    return { days, hours, mins, secs };
+    return { total, days, hours, mins, secs };
   }
 
   timer() {
     const startTime = this.targetDate;
 
-    setInterval(() => {
+    let timeInterval = setInterval(() => {
       const currentTime = Date.now();
       const time = this.getTimerComponents(startTime - currentTime);
+      if (time.total <= 0) {
+        clearInterval(timeInterval);
+        return;
+      }
       const arrayContainer = [...this.selector.children];
       arrayContainer.map((el) => {
         switch (el.firstElementChild.dataset.value) {
